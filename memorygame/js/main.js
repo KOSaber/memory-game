a85495e4fd7d1b07acd4e7e7bf8a33f2f3fcdeb4
KOSaber <57635071+KOSaber@users.noreply.github.com>
2019-11-14 10:45:28 +0300

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,8 +1,28 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+
 //console.log()
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+var cards =["queen","queen","king","king"];
+var cardsInPlay;
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardThree = cards[1];
+var cardFour = cards[4];
+
+
+cardsInPlay.push(cardOne);
+console.log;
+
+cardsInPlay.push (cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length===2) {
+	console.log;
+}else if (cardsInPlay[0]===cardsInPlay[1]) {
+alert("You found a match!");
+}else{
+	alert( "Sorry, try again.");
+}
+
